3c18f05efb2a7a604d3a8a7c9a48ed183cc6e0a5
Oleg <root-avanzar@mail.ua>
2020-05-03 22:42:18 +0300

    delete spam

--- jackherer.bong/app/static/php/send_mail.php
+++ jackherer.bong/app/static/php/send_mail.php
@@ -4,16 +4,16 @@
 	$email = $_POST["email"];
 	$text = $_POST["text"];
 
-	$to = "gsk.2013@mail.ru";
+	$to = "...";
 
-	$subject = "Новая заявка с сайта: kvartirayalta.com";
+	$subject = "Новая заявка с сайта: ...";
 
 	$message = "Имя пользователя: ".htmlspecialchars($name)."<br />
 				Телефон: ".htmlspecialchars($phone)."<br />
 				Почта: ".htmlspecialchars($email)."<br />
 				".htmlspecialchars($text);
 
-	$headers = "From: kvartirayalta.com <".htmlspecialchars($email).">\r\nContent-type: text/html; charset=UTF-8 \r\n";
+	$headers = "From: ... <".htmlspecialchars($email).">\r\nContent-type: text/html; charset=UTF-8 \r\n";
 
 	mail($to, $subject, $message, $headers);
 	exit();
